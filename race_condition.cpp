37152477fa8d0981938a87b67ee45c058a5fc119
aseelFraira <asi.shib69@gmail.com>
2024-12-26 17:26:45 +0200

    Adding race condtion

--- race_condition.cpp
+++ race_condition.cpp
@@ -15,13 +15,13 @@ int main() {
     pthread_t new_thread; // Holds the ID of the new thread
 
     // Create a new thread
-    pthread_create(&new_thread, NULL, count_huge, NULL);
+  //  pthread_create(&new_thread, NULL, count_huge, NULL);
 
     // Run in main thread
     count_huge(NULL);
 
     // Wait for the new thread to finish
-    pthread_join(new_thread, NULL);
+   // pthread_join(new_thread, NULL);
 
     // Print the final counter value
     printf("Final counter: %d\n", counter);
