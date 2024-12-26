a832c1895d54b2ad94347db180df157658c7d62d
aseelFraira <asi.shib69@gmail.com>
2024-12-26 17:29:56 +0200

    Adding race condtion

--- race_condition.cpp
+++ race_condition.cpp
@@ -3,25 +3,31 @@
 
 #define HUGE 10000000
 int counter = 0;
+pthread_mutex_t lock;
 
 void* count_huge(void* arg) {
     for (int i = 0; i < HUGE; ++i) {
+        pthread_mutex_lock(&lock);
         counter++;
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
 
 int main() {
     pthread_t new_thread; // Holds the ID of the new thread
+    pthread_mutex_init(&lock, NULL);
 
-    // Create a new thread
-  //  pthread_create(&new_thread, NULL, count_huge, NULL);
+    pthread_create(&new_thread, NULL, count_huge, NULL);
 
     // Run in main thread
     count_huge(NULL);
 
     // Wait for the new thread to finish
-   // pthread_join(new_thread, NULL);
+    pthread_join(new_thread, NULL);
+
+
+    pthread_mutex_destroy(&lock);
 
     // Print the final counter value
     printf("Final counter: %d\n", counter);
