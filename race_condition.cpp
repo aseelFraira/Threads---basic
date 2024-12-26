945b661129952ad92bf8488173828946759e73dc
aseelFraira <asi.shib69@gmail.com>
2024-12-26 17:41:15 +0200

    Adding race condtion

--- race_condition.cpp
+++ race_condition.cpp
@@ -6,7 +6,7 @@ int counter = 0;
 pthread_mutex_t lock;
 
 void* count_huge(void* arg) {
-    for (int i = 0; i < HUGE; ++i) {
+    for (int i = 0; i < HUGE && counter<HUGE; ++i) {
         pthread_mutex_lock(&lock);
         counter++;
         pthread_mutex_unlock(&lock);
